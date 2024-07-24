f4d31b3183c184f36141f368d8d56e720ead88e5
Aline Andreaça <54520161+Alineandreaca@users.noreply.github.com>
2024-07-24 18:29:54 -0300

    Update teste2.py

--- aplicacao/teste2.py
+++ aplicacao/teste2.py
@@ -15,9 +15,9 @@ import altair as alt
 
 
 app = Dash(__name__)
-server = app.server
+#server = app.server
 port = int(os.environ.get("PORT", 5000))
-server.run(host='0.0.0.0', port=port)
+app.server.run(host='0.0.0.0', port=port)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
